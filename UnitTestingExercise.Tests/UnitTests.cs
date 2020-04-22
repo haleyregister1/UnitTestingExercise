e483d48181a49ea4365ba9af029dd4bf22f47ff4
Haley Register <haleyreg@gmail.com>
2020-04-22 13:45:05 -0500

    completed project, all tests passed

--- UnitTestingExercise.Tests/UnitTests.cs
+++ UnitTestingExercise.Tests/UnitTests.cs
@@ -1,77 +1,96 @@
 using System;
 using Xunit;
+using UnitTestingExercise;
 
 namespace UnitTestingExercise.Tests
 {
     public class UnitTests
     {
         [Theory]
-        [InlineData()]
+        [InlineData(10, 20, 30, 60)]
         public void Add(int num1, int num2, int num3, int expected)
         {
             //Arrange
+            var sut = new UnitTestMethods();
 
             //Act
+            var actual = sut.AddMethod(num1, num2, num3);
 
             //Assert
-
+            Assert.Equal(actual, expected);
         }
 
         [Theory]
-        [InlineData()]
+        [InlineData(12, 8, 4)]
         public void Subtract(int minuend, int subtrhend, int expected)
         {
             //Arrange
+            var sut = new UnitTestMethods();
 
             //Act
+            var actual = sut.SubtractMethod(minuend, subtrhend);
 
             //Assert
-
+            Assert.Equal(actual, expected);
         }
 
         [Theory]
-        [InlineData()]
+        [InlineData(5, 4, 20)]
         public void Multiply(int num1, int num2, int expected)
         {
             //Arrange
+            var sut = new UnitTestMethods();
 
             //Act
+            var actual = sut.MultiMethod(num1, num2);
 
             //Assert
-
+            Assert.Equal(actual, expected);
         }
 
         [Theory]
-        [InlineData()]
+        [InlineData(20, 5, 4)]
         public void Divide(int num1, int num2, int expected)
         {
             //Arrange
+            var sut = new UnitTestMethods();
 
             //Act
+            var actual = sut.DivideMethod(num1, num2);
 
             //Assert
+            Assert.Equal(actual, expected);
 
         }
 
         [Fact]
-        public void YourMethodName()
+        public void CheckFactor()
         {
             //Arrange
-
+            var sut = new UnitTestMethods();
+            int num1 = 12;
+            int num2 = 3;
+            string expected = "They factor into each other!";
             //Act
+            var actual = sut.FactorTest(num1, num2);
 
             //Assert
-
+            Assert.Equal(actual, expected);
         }
 
         [Fact]
-        public void YourMethodName2()
+        public void EvenOrOddMethod()
         {
             //Arrange
+            var sut = new UnitTestMethods();
+            int num = 8;
+            string expected = "It is even!";
 
             //Act
+            var actual = sut.EvenOrOdd(num);
 
             //Assert
+            Assert.Equal(actual, expected);
         }
     }
 }
--- UnitTestingExercise/UnitTestMethods.cs
+++ UnitTestingExercise/UnitTestMethods.cs
@@ -9,18 +9,55 @@ namespace UnitTestingExercise
 
 
         // Create an Add method that passes 3 integers
-
+        public int AddMethod(int num1, int num2, int num3)
+        {
+            int sum = num1 + num2 + num3;
+            return sum;
+        }
         // Create a Subtract method that passes 2 integers
             // Keep track of which number is getting passed as minuend and subtrahend
-
+        public int SubtractMethod(int min1, int sub2)
+        {
+            int diff = min1 - sub2;
+            return diff;
+        }
         // Create a Multiply method that passes 2 integers
-
-        // Create a Divide method that passes 2 integers
-
+        public int MultiMethod(int num1, int num2)
+        {
+            int prod = num1 * num2;
+            return prod;
+        }
+        // Create a Divide method that passes 2 integers\
+        public int DivideMethod(int num1, int num2)
+        {
+            int quo = num1 / num2;
+            return quo;
+        }
 
 
         // Create 2 methods that will utilize the [Fact] tests you wrote
-
+        public string FactorTest(int num1, int num2)
+        {
+            if(num1 % num2 == 0)
+            {
+                return "They factor into each other!";
+            }
+            else
+            {
+                return "They do not factor";
+            }
+        }
+        public string EvenOrOdd(int num1)
+        {
+            if(num1 % 2 == 0)
+            {
+                return "It is even!";
+            }
+            else
+            {
+                return "It is odd";
+            }
+        }
         
     }
 }
